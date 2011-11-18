26873d4a03d2f9d893caf17847e12c1e9d9f6ae8
Kazuhiro SERA <seratch@gmail.com>
2011-11-18 16:45:17 +0900

    version 0.1.1

--- project/build.scala
+++ project/build.scala
@@ -9,7 +9,7 @@ object ScalikeJDBCBuild extends Build {
     sbtPlugin := false,
     organization := "com.github.seratch",
     name := "scalikejdbc",
-    version := "0.1.0",
+    version := "0.1.1",
     publishTo := Some(
       Resolver.file(
         "Github Pages",
