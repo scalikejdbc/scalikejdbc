4a2cf97d6c6f67870db20cfcb10dcd5241a8d00a
Kazuhiro Sera <seratch@gmail.com>
2012-08-24 22:53:40 +0900

    Added test for here document values

--- scalikejdbc-interpolation/src/test/scala/scalikejdbc/SQLInterpolationSpec.scala
+++ scalikejdbc-interpolation/src/test/scala/scalikejdbc/SQLInterpolationSpec.scala
@@ -3,6 +3,8 @@ package scalikejdbc
 import org.scalatest._
 import org.scalatest.matchers._
 
+import scalikejdbc.SQLInterpolation._
+
 class SQLInterpolationSpec extends FlatSpec with ShouldMatchers {
 
   behavior of "SQLInterpolation"
@@ -13,23 +15,45 @@ class SQLInterpolationSpec extends FlatSpec with ShouldMatchers {
   case class User(id: Int, name: String)
 
   it should "be available" in {
-
-    import scalikejdbc.SQLInterpolation._
-
-    DB localTx {
+   DB localTx {
       implicit s =>
         try {
-          sql"create table users (id int, name varchar(256));".execute.apply()
-          sql"insert into users values (${1}, ${"foo"})".update.apply()
-          sql"insert into users values (${2}, ${"bar"})".update.apply()
-          sql"insert into users values (${3}, ${"baz"})".update.apply()
+          sql"create table users (id int, name varchar(256))".execute.apply()
+
+          Seq((1, "foo"),(2, "bar"), (3, "baz")) foreach { case (id, name) =>
+            sql"insert into users values (${id}, ${name})".update.apply()
+          }
+
           val id = 3
           val user = sql"select * from users where id = ${id}".map {
             rs => User(id = rs.int("id"), name = rs.string("name"))
           }.single.apply()
           user.isDefined should equal(true)
+          user.get.id should equal(3)
+          user.get.name should equal("baz")
         } finally {
-          sql"drop table users;".execute.apply()
+          sql"drop table users".execute.apply()
+        }
+    }
+  }
+
+  it should "be available with here document values" in {
+    DB localTx {
+      implicit s =>
+        try {
+          sql"""create table users (id int, name varchar(256))""".execute.apply()
+
+          Seq((1, "foo"),(2, "bar"), (3, "baz")) foreach { case (id, name) =>
+            sql"""insert into users values (${id}, ${name})""".update.apply()
+          }
+
+          val id = 3
+          val user = sql"""select * from users where id = ${id}""".map {
+            rs => User(id = rs.int("id"), name = rs.string("name"))
+          }.single.apply()
+          user.isDefined should equal(true)
+        } finally {
+          sql"""drop table users""".execute.apply()
         }
     }
   }
