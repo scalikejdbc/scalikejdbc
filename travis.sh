211b90f07d8f88067202b772b9e38aaf2f413b34
Kazuhiro Sera <seratch@gmail.com>
2012-10-17 18:47:50 +0900

    Updated the script for Travis CI

--- travis.sh
+++ travis.sh
@@ -5,6 +5,6 @@ wget https://raw.github.com/paulp/sbt-extras/master/sbt &&
 chmod u+x ./sbt &&
 ./sbt -sbt-version 0.12.0 -mem 512 \
   "project library" test \
-  "project mapper-generator" test scripted \
-  "project play-plugin" test 
+  "project mapper-generator" test scripted
+  # "project play-plugin" test # Travis CI can't execute this task normally
 
