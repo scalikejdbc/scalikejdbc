5c57c5e6bee0311dce9a2c49cf017edcbed8b12f
Kazuhiro Sera <seratch@gmail.com>
2012-10-19 16:25:04 +0900

    Fixed an invalid test

--- scalikejdbc-library/src/test/scala/scalikejdbc/ConnectionPoolContextSpec.scala
+++ scalikejdbc-library/src/test/scala/scalikejdbc/ConnectionPoolContextSpec.scala
@@ -5,8 +5,11 @@ import org.scalatest.matchers._
 
 object ConnectionPoolContextSpecUtils {
 
-  def createTable(tableName: String) = {
-    DB autoCommit { implicit s =>
+  Class.forName("org.h2.Driver")
+  ConnectionPool.add('ConnectionPoolContextSpec, "jdbc:h2:mem:ConnectionPoolContextSpec", "", "")
+
+  def createTable(tableName: String)(name: Any) = {
+    NamedDB(name)(NoConnectionPoolContext) autoCommit { implicit s =>
       try {
         SQL("drop table " + tableName).execute.apply()
       } catch { case e => }
@@ -14,17 +17,17 @@ object ConnectionPoolContextSpecUtils {
     }
   }
 
-  def insertData(tableName: String, num: Int) = {
-    DB localTx { implicit s =>
+  def insertData(tableName: String, num: Int)(name: Any) = {
+    NamedDB(name)(NoConnectionPoolContext) localTx { implicit s =>
       (1 to num).foreach { n =>
         SQL("insert into " + tableName + " (id, name) values (?, ?)").bind(n, "name" + n).update.apply()
       }
     }
   }
 
-  def dropTable(tableName: String) = {
+  def dropTable(tableName: String)(name: Any) = {
     try {
-      DB autoCommit { implicit s =>
+      NamedDB(name)(NoConnectionPoolContext) autoCommit { implicit s =>
         SQL("drop table " + tableName).execute.apply()
       }
     } catch { case e => }
@@ -33,11 +36,13 @@ object ConnectionPoolContextSpecUtils {
 }
 
 trait NamedCPContextAsDefault {
-  implicit val context = new MultipleConnectionPoolContext
-  context.set('named, ConnectionPool())
+  implicit val context = MultipleConnectionPoolContext(
+    ConnectionPool.DEFAULT_NAME -> ConnectionPool.get(),
+    'ConnectionPoolContextSpec -> ConnectionPool.get()
+  )
 }
 
-class ConnectionPoolContextMixinSpec extends FlatSpec with ShouldMatchers with Settings {
+class ConnectionPoolContextMixinSpec extends FlatSpec with ShouldMatchers with Settings with NamedCPContextAsDefault {
 
   import ConnectionPoolContextSpecUtils._
 
@@ -48,8 +53,9 @@ class ConnectionPoolContextMixinSpec extends FlatSpec with ShouldMatchers with S
   it should "work with NamedConnectionPoolContext" in {
     val tableName = tableNamePrefix + "_withNamedCPContextMixin"
     try {
-      createTable(tableName)
-      insertData(tableName, 6)
+      createTable(tableName)(ConnectionPool.DEFAULT_NAME)
+      createTable(tableName)('ConnectionPoolContextSpec)
+      insertData(tableName, 6)(ConnectionPool.DEFAULT_NAME)
 
       val result1 = DB readOnly { implicit s =>
         SQL("select * from " + tableName).map(rs => rs.string("name")).list.apply()
@@ -62,14 +68,15 @@ class ConnectionPoolContextMixinSpec extends FlatSpec with ShouldMatchers with S
       result11.size should equal(6)
       result1.zip(result11).foreach { case (a, b) => a should equal(b) }
 
-      val result2 = NamedDB('named)(NoConnectionPoolContext) readOnly { implicit s =>
+      val result2 = NamedDB('ConnectionPoolContextSpec) readOnly { implicit s =>
         SQL("select * from " + tableName).map(rs => rs.string("name")).list.apply()
       }
       result2.size should equal(6)
       result1.zip(result2).foreach { case (a, b) => a should equal(b) }
 
     } finally {
-      dropTable(tableName)
+      dropTable(tableName)(ConnectionPool.DEFAULT_NAME)
+      dropTable(tableName)('ConnectionPoolContextSpec)
     }
   }
 
@@ -86,15 +93,16 @@ class ConnectionPoolContextSpec extends FlatSpec with ShouldMatchers with Settin
   it should "work with NoConnectionPoolContext" in {
     val tableName = tableNamePrefix + "_withNoCPContext"
     try {
-      createTable(tableName)
-      insertData(tableName, 4)
+      createTable(tableName)(ConnectionPool.DEFAULT_NAME)
+      createTable(tableName)('ConnectionPoolContextSpec)
+      insertData(tableName, 4)(ConnectionPool.DEFAULT_NAME)
 
       val result1 = DB readOnly { implicit s =>
         SQL("select * from " + tableName).map(rs => rs.string("name")).list.apply()
       }
       result1.size should equal(4)
 
-      val result11 = NamedDB(ConnectionPool.DEFAULT_NAME) readOnly { implicit s =>
+      val result11 = NamedDB(ConnectionPool.DEFAULT_NAME)(NoConnectionPoolContext) readOnly { implicit s =>
         SQL("select * from " + tableName).map(rs => rs.string("name")).list.apply()
       }
       result11.size should equal(4)
@@ -107,49 +115,21 @@ class ConnectionPoolContextSpec extends FlatSpec with ShouldMatchers with Settin
       result1.zip(result2).foreach { case (a, b) => a should equal(b) }
 
     } finally {
-      dropTable(tableName)
-    }
-  }
-
-  it should "work with DefaultConnectionPoolContext" in {
-    val tableName = tableNamePrefix + "_withDefaultCPContext"
-    implicit val context = MultipleConnectionPoolContext(
-      ConnectionPool.DEFAULT_NAME -> ConnectionPool()
-    )
-    try {
-      createTable(tableName)
-      insertData(tableName, 5)
-
-      val result1 = DB readOnly { implicit s =>
-        SQL("select * from " + tableName).map(rs => rs.string("name")).list.apply()
-      }
-      result1.size should equal(5)
-
-      val result11 = NamedDB(ConnectionPool.DEFAULT_NAME) readOnly { implicit s =>
-        SQL("select * from " + tableName).map(rs => rs.string("name")).list.apply()
-      }
-      result11.size should equal(5)
-      result1.zip(result11).foreach { case (a, b) => a should equal(b) }
-
-      val result2 = NamedDB(ConnectionPool.DEFAULT_NAME)(NoConnectionPoolContext) readOnly { implicit s =>
-        SQL("select * from " + tableName).map(rs => rs.string("name")).list.apply()
-      }
-      result2.size should equal(5)
-      result1.zip(result2).foreach { case (a, b) => a should equal(b) }
-
-    } finally {
-      dropTable(tableName)
+      dropTable(tableName)(ConnectionPool.DEFAULT_NAME)
+      dropTable(tableName)('ConnectionPoolContextSpec)
     }
   }
 
   it should "work with NamedConnectionPoolContext" in {
     val tableName = tableNamePrefix + "_withNamedCPContext"
     implicit val context = MultipleConnectionPoolContext(
-      ConnectionPool.DEFAULT_NAME -> ConnectionPool()
+      ConnectionPool.DEFAULT_NAME -> ConnectionPool.get(),
+      'ConnectionPoolContextSpec -> ConnectionPool.get()
     )
     try {
-      createTable(tableName)
-      insertData(tableName, 6)
+      createTable(tableName)(ConnectionPool.DEFAULT_NAME)
+      createTable(tableName)('ConnectionPoolContextSpec)
+      insertData(tableName, 6)(ConnectionPool.DEFAULT_NAME)
 
       val result1 = DB readOnly { implicit s =>
         SQL("select * from " + tableName).map(rs => rs.string("name")).list.apply()
@@ -162,14 +142,15 @@ class ConnectionPoolContextSpec extends FlatSpec with ShouldMatchers with Settin
       result11.size should equal(6)
       result1.zip(result11).foreach { case (a, b) => a should equal(b) }
 
-      val result2 = NamedDB('named)(NoConnectionPoolContext) readOnly { implicit s =>
+      val result2 = NamedDB('ConnectionPoolContextSpec) readOnly { implicit s =>
         SQL("select * from " + tableName).map(rs => rs.string("name")).list.apply()
       }
       result2.size should equal(6)
       result1.zip(result2).foreach { case (a, b) => a should equal(b) }
 
     } finally {
-      dropTable(tableName)
+      dropTable(tableName)(ConnectionPool.DEFAULT_NAME)
+      dropTable(tableName)('ConnectionPoolContextSpec)
     }
   }
 
