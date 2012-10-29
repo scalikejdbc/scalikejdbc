3582df0be7b9581e91a080a3cc466eb3a22451b0
Kazuhiro Sera <seratch@gmail.com>
2012-10-29 23:08:26 +0900

    Added tests for StringSQLRunner

--- scalikejdbc-library/src/main/scala/scalikejdbc/StringSQLRunner.scala
+++ scalikejdbc-library/src/main/scala/scalikejdbc/StringSQLRunner.scala
@@ -1,5 +1,35 @@
+/*
+ * Copyright 2012 Kazuhiro Sera
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package scalikejdbc
 
+/**
+ * String SQL Runner
+ *
+ * Basic Usage:
+ *
+ * {{{
+ * import scalikejdbc.StringSQLRunner._
+ *
+ * val result: List[Map[String, Any]] = "insert into users values (1, 'Alice')".run()
+ *
+ * val users: List[Map[String, Any]] = "select * from users".run()
+ * }}}
+ *
+ * @param sql SQL value
+ */
 case class StringSQLRunner(sql: String) {
 
   def run()(implicit session: DBSession = AutoSession): List[Map[String, Any]] = try {
---
+++ scalikejdbc-library/src/test/scala/scalikejdbc/StringSQLRunnerSpec.scala
@@ -0,0 +1,30 @@
+package scalikejdbc
+
+import org.scalatest._
+import org.scalatest.matchers._
+import scala.util.control.Exception._
+
+class StringSQLRunnerSpec extends FlatSpec with ShouldMatchers {
+
+  val tableNamePrefix = "emp_StringSQLRunnerSpec" + System.currentTimeMillis()
+
+  behavior of "StringSQLRunner"
+
+  it should "be available" in {
+    val tableName = tableNamePrefix + "_beAvailable"
+    ultimately(TestUtils.deleteTable(tableName)) {
+      TestUtils.initialize(tableName)
+      using(new DB(ConnectionPool.borrow())) { db =>
+        implicit val session = db.autoCommitSession()
+
+        import scalikejdbc.StringSQLRunner._
+
+        ("insert into " + tableName + " values (3, 'Ben')").run
+        val result = ("select id,name from " + tableName + " where id = 3").run
+        result.head.get("ID").get should equal(3)
+        result.head.get("NAME").get should equal("Ben")
+      }
+    }
+  }
+
+}
