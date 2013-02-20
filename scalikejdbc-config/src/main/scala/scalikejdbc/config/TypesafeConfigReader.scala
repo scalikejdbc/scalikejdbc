bf0ede849c482f1e2accf572555ede727ddf6ca2
Kazuhiro Sera <seratch@gmail.com>
2013-02-20 22:17:20 +0900

    Added CPSettings, GlobalSettings support Fixed #69 [config] Reading ConnectionPoolSettings Fixed #70 [config] Reading GlobalSettings

--- scalikejdbc-config/src/main/scala/scalikejdbc/config/TypesafeConfigReader.scala
+++ scalikejdbc-config/src/main/scala/scalikejdbc/config/TypesafeConfigReader.scala
@@ -1,16 +1,37 @@
+/*
+ * Copyright 2013 Toshiyuki Takahashi, Kazuhiro Sera
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package scalikejdbc.config
 
 import scalikejdbc._
 import com.typesafe.config.{ Config => TypesafeConfig, ConfigFactory, ConfigException }
 import scala.collection.mutable.{ Map => MutableMap, ListBuffer }
 
+/**
+ * Configuration Exception
+ */
 class ConfigurationException(val message: String) extends Exception(message) {
   def this(e: Throwable) = this(e.getMessage)
 }
 
+/**
+ * Typesafe Config reader
+ */
 object TypesafeConfigReader {
 
-  lazy val _config: TypesafeConfig = ConfigFactory.load()
+  private lazy val _config: TypesafeConfig = ConfigFactory.load()
 
   lazy val dbNames: List[String] = {
     val it = _config.entrySet.iterator
@@ -19,35 +40,29 @@ object TypesafeConfigReader {
       val entry = it.next
       val key = entry.getKey
       key.split("\\.").toList match {
-        case List("db", dbName, _) => {
-          buf.append(dbName)
-        }
+        case List("db", dbName, _) => buf.append(dbName)
         case _ => ()
       }
     }
     buf.toList.distinct
   }
 
-  def readAsMap(dbName: Symbol): Map[String, String] = {
-    try {
-      val dbConfig = _config.getConfig("db." + dbName.name)
-      val it = dbConfig.entrySet.iterator
-      val configMap: MutableMap[String, String] = MutableMap.empty
-      while (it.hasNext) {
-        val entry = it.next
-        val key = entry.getKey
-        configMap(key) = _config.getString("db." + dbName.name + "." + key)
-      }
-      configMap.toMap
-    } catch {
-      case e: ConfigException => throw new ConfigurationException(e)
-      case e: ConfigException => throw new ConfigurationException(e)
+  def readAsMap(dbName: Symbol = ConnectionPool.DEFAULT_NAME): Map[String, String] = try {
+    val dbConfig = _config.getConfig("db." + dbName.name)
+    val iter = dbConfig.entrySet.iterator
+    val configMap: MutableMap[String, String] = MutableMap.empty
+    while (iter.hasNext) {
+      val entry = iter.next()
+      val key = entry.getKey
+      configMap(key) = _config.getString("db." + dbName.name + "." + key)
     }
+    configMap.toMap
+  } catch {
+    case e: ConfigException => throw new ConfigurationException(e)
   }
 
-  def readJDBCSettings(dbName: Symbol): JDBCSettings = {
+  def readJDBCSettings(dbName: Symbol = ConnectionPool.DEFAULT_NAME): JDBCSettings = {
     val configMap = TypesafeConfigReader.readAsMap(dbName)
-
     (for {
       driver <- configMap.get("driver")
       url <- configMap.get("url")
@@ -57,6 +72,34 @@ object TypesafeConfigReader {
       JDBCSettings(driver, url, user, password)
     }) getOrElse {
       throw new ConfigurationException("Configuration error for database " + dbName + ". " + configMap.toString)
+    }
+  }
+
+  def readConnectionPoolSettings(dbName: Symbol = ConnectionPool.DEFAULT_NAME): ConnectionPoolSettings = {
+     val configMap = TypesafeConfigReader.readAsMap(dbName)
+     val default = new ConnectionPoolSettings
+     ConnectionPoolSettings(
+       initialSize = configMap.get("poolInitialSize").map(_.toInt).getOrElse(default.initialSize),
+       maxSize = configMap.get("poolMaxSize").map(_.toInt).getOrElse(default.maxSize),
+       validationQuery = configMap.get("poolValidationQuery").getOrElse(default.validationQuery)
+     )
+  }
+
+  def loadGlobalSettings(): Unit = Option(_config.getConfig("scalikejdbc.global")).map { globalConfig =>
+    Option(globalConfig.getConfig("loggingSQLAndTime")).map { config => 
+      val enabled = config.getBoolean("enabled")
+      if (enabled) {
+        val default = LoggingSQLAndTimeSettings()
+        GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(
+          enabled = enabled,
+          logLevel = Option(config.getString("logLevel")).map(v => Symbol(v)).getOrElse(default.logLevel),
+          warningEnabled = Option(config.getString("warningEnabled")).map(_.toBoolean).getOrElse(default.warningEnabled),
+          warningThresholdMillis = Option(config.getString("warningThresholdMillis")).map(_.toLong).getOrElse(default.warningThresholdMillis),
+          warningLogLevel = Option(config.getString("warningLogLevel")).map(v => Symbol(v)).getOrElse(default.warningLogLevel)
+        )
+      } else {
+        GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(enabled = false)
+      }
     }
   }
 
--- scalikejdbc-config/src/test/scala/scalikejdbc/config/TypesafeConfigReaderSpec.scala
+++ scalikejdbc-config/src/test/scala/scalikejdbc/config/TypesafeConfigReaderSpec.scala
@@ -6,13 +6,14 @@ import scalikejdbc._
 
 class TypesafeConfigReaderSpec extends FunSpec with ShouldMatchers {
 
-  def fixture = new {
-
-  }
-
   describe("TypesafeConfigReader") {
 
     describe ("#readJDBCSettings") {
+
+      it ("should read configuration and return as JDBCSettings") {
+        val expected = JDBCSettings("org.h2.Driver", "jdbc:h2:mem:test1", "sa", "secret")
+        TypesafeConfigReader.readJDBCSettings() should be (expected)
+      }
 
       it ("should read configuration by db name and return as JDBCSettings") {
         val expected = JDBCSettings("org.h2.Driver", "jdbc:h2:mem:test2", "sa", "secret")
@@ -35,7 +36,10 @@ class TypesafeConfigReaderSpec extends FunSpec with ShouldMatchers {
           "driver" -> "org.h2.Driver",
           "url" -> "jdbc:h2:mem:test2",
           "user" -> "sa",
-          "password" -> "secret"
+          "password" -> "secret",
+          "poolInitialSize" -> "1",
+          "poolMaxSize" -> "2",
+          "poolValidationQuery" -> "select 1 as foo"
         )
         TypesafeConfigReader.readAsMap('foo) should be (expected)
       }
@@ -54,6 +58,38 @@ class TypesafeConfigReaderSpec extends FunSpec with ShouldMatchers {
       val expected = List("default", "foo", "bar").sorted
       TypesafeConfigReader.dbNames.sorted should be (expected)
     }
+
+    describe ("#readConnectionPoolSettings") {
+
+      it ("should read configuration and return as ConnectionPoolSettings") {
+        val expected = ConnectionPoolSettings(5, 7, "select 1 as one")
+        TypesafeConfigReader.readConnectionPoolSettings() should be (expected)
+      }
+
+      it ("should read configuration for foo db and return as ConnectionPoolSettings") {
+        val expected = ConnectionPoolSettings(1, 2, "select 1 as foo")
+        TypesafeConfigReader.readConnectionPoolSettings('foo) should be (expected)
+      }
+
+      it ("should read configuration for bar db and return as ConnectionPoolSettings") {
+        val expected = ConnectionPoolSettings(2, 3, "select 1 as bar")
+        TypesafeConfigReader.readConnectionPoolSettings('bar) should be (expected)
+      }
+
+      describe ("When an unknown database name is passed") {
+        it ("throws Configuration Exception") {
+          intercept[ConfigurationException] {
+            TypesafeConfigReader.readConnectionPoolSettings('unknown)
+          }
+        }
+      }
+
+    }
+
+    it ("should load global settings") {
+      TypesafeConfigReader.loadGlobalSettings() 
+    }
+
   }
 
 }
