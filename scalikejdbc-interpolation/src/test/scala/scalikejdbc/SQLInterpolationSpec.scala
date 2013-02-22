85ab3d5fdc4116a0d8f9b5140710910926d0e8ea
Kazuhiro Sera <seratch@gmail.com>
2013-02-22 21:45:26 +0900

    Added a simple join test case

--- scalikejdbc-interpolation/src/test/scala/scalikejdbc/SQLInterpolationSpec.scala
+++ scalikejdbc-interpolation/src/test/scala/scalikejdbc/SQLInterpolationSpec.scala
@@ -22,30 +22,52 @@ class SQLInterpolationSpec extends FlatSpec with ShouldMatchers {
 
   object User extends SQLSyntaxSupport {
     override def tableName = "users"
-    override def columns = Seq("id", "first_name")
+    override def columns = Seq("id", "first_name", "group_id")
     override def forceUpperCase = true
   }
 
-  case class User(id: Int, name: String)
+  case class User(id: Int, name: Option[String], groupId: Option[Int] = None, group: Option[Group] = None)
+
+  object Group extends SQLSyntaxSupport {
+    override def tableName = "groups"
+    override def columns = Seq("id", "website_url")
+  }
+  case class Group(id: Int, websiteUrl: Option[String])
 
   it should "be available with SQLSyntaxSupport" in {
    DB localTx {
       implicit s =>
         try {
-          sql"create table users (id int, first_name varchar(256))".execute.apply()
+          sql"create table users (id int not null, first_name varchar(256), group_id int)".execute.apply()
+          sql"create table groups (id int not null, website_url varchar(256))".execute.apply()
 
-          Seq((1, "foo"),(2, "bar"), (3, "baz")) foreach { case (id, name) =>
-            sql"insert into users values (${id}, ${name})".update.apply()
+          Seq((1, Some("foo"), None),(2, Some("bar"), None), (3, Some("baz") ,Some(1))) foreach { case (id, name, groupId) =>
+            sql"insert into users values (${id}, ${name}, ${groupId})".update.apply()
           }
+          sql"insert into groups values (${1}, ${"http://www.example.com"})".update.apply()
 
           val id = 3
           val u = User.syntax("u")
-          val user = sql"select ${u.result.*} from ${User.as(u)} where ${u.id} = ${id}".map {
-            rs => User(id = rs.int(u.result.id), name = rs.string(u.result.firstName))
+          val g = Group.syntax("g")
+          val user = sql"""
+            select ${u.result.*}, ${g.result.*} 
+            from ${User.as(u)} left join ${Group.as(g)} 
+            on ${u.groupId} = ${g.id}
+            where ${u.id} = ${id}
+            """.map { rs => 
+              User(
+                id = rs.int(u.result.id), 
+                name = rs.stringOpt(u.result.firstName), 
+                groupId = rs.intOpt(u.result.groupId),
+                group = rs.intOpt(g.result.id).map(id => Group(id = id, websiteUrl = rs.stringOpt(g.result.websiteUrl))
+              )
+            )
           }.single.apply()
           user.isDefined should equal(true)
           user.get.id should equal(3)
-          user.get.name should equal("baz")
+          user.get.name should equal(Some("baz"))
+
+          user.get.group.isDefined should equal(true)
 
           intercept[IllegalArgumentException] {
             val user = sql"select ${u.result.*} from ${User.as(u)} where ${u.id} = ${id}".map { rs => u.result.dummy  }.single.apply()
@@ -69,7 +91,7 @@ class SQLInterpolationSpec extends FlatSpec with ShouldMatchers {
 
           val id = 3
           val user = sql"""select * from users where id = ${id}""".map {
-            rs => User(id = rs.int("id"), name = rs.string("name"))
+            rs => User(id = rs.int("id"), name = rs.stringOpt("name"))
           }.single.apply()
           user.isDefined should equal(true)
         } finally {
@@ -90,11 +112,11 @@ class SQLInterpolationSpec extends FlatSpec with ShouldMatchers {
 
           val id = 2 
           val user = sql"select * from users where id = ${id}".map {
-            rs => User(id = rs.int("id"), name = rs.string("name"))
+            rs => User(id = rs.int("id"), name = rs.stringOpt("name"))
           }.single.apply()
           user.isDefined should equal(true)
           user.get.id should equal(2)
-          user.get.name should be(null)
+          user.get.name should be(None)
         } finally {
           sql"drop table users".execute.apply()
         }
@@ -112,10 +134,10 @@ class SQLInterpolationSpec extends FlatSpec with ShouldMatchers {
 
           val ids = List(1, 2, 4) ::: (100 until 200).toList
           val users = sql"select * from users where id in (${ids})".map {
-            rs => User(id = rs.int("id"), name = rs.string("name"))
+            rs => User(id = rs.int("id"), name = rs.stringOpt("name"))
           }.list.apply()
           users.size should equal(2)
-          users.map(_.name) should equal(Seq("foo", "bar"))
+          users.map(_.name) should equal(Seq(Some("foo"), Some("bar")))
         } finally {
           sql"drop table users".execute.apply()
         }
@@ -134,10 +156,10 @@ class SQLInterpolationSpec extends FlatSpec with ShouldMatchers {
           val ids = List(1, 2, 4) ::: (100 until 200).toList
           val sorting = SQLSyntax("DESC")
           val users = sql"select * from users where id in (${ids}) order by id ${sorting}".map {
-            rs => User(id = rs.int("id"), name = rs.string("name"))
+            rs => User(id = rs.int("id"), name = rs.stringOpt("name"))
           }.list.apply()
           users.size should equal(2)
-          users.map(_.name) should equal(Seq("bar", "foo"))
+          users.map(_.name) should equal(Seq(Some("bar"), Some("foo")))
         } finally {
           sql"drop table users".execute.apply()
         }
