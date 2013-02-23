d8cab686c6b95383d13099f43918285db58aa17d
Kazuhiro Sera <seratch@gmail.com>
2013-02-23 13:31:48 +0900

    Updated release script

--- scripts/release_2.10.0.sh
+++ scripts/release_2.10.0.sh
@@ -3,24 +3,9 @@
 cd `dirname $0`/..
 
 sbt \
-  "project library" \
-  "set scalaVersion := \"2.10.0\"" \
-  "set scalaBinaryVersion := \"2.10\"" \
-  publish \
-  "project config" \
-  "set scalaVersion := \"2.10.0\"" \
-  "set scalaBinaryVersion := \"2.10\"" \
-  publish \
-  "project interpolation" \
-  "set scalaVersion := \"2.10.0\"" \
-  "set scalaBinaryVersion := \"2.10\"" \
-  publish \
-  "project play-plugin" \
-  "set scalaVersion := \"2.10.0\"" \
-  "set scalaBinaryVersion := \"2.10\"" \
-  publish \
-  "project test" \
-  "set scalaVersion := \"2.10.0\"" \
-  "set scalaBinaryVersion := \"2.10\"" \
-  publish 
+  "project library" ++2.10.0 publish \
+  "project config" ++2.10.0 publish \
+  "project interpolation" ++2.10.0 publish \
+  "project play-plugin" ++2.10.0 publish \
+  "project test" ++2.10.0 publish 
 
--- scripts/release_2.9.1.sh
+++ scripts/release_2.9.1.sh
@@ -3,20 +3,8 @@
 cd `dirname $0`/..
 
 sbt \
-  "project library" \
-  "set scalaVersion := \"2.9.1\"" \
-  "set scalaBinaryVersion := \"2.9.1\"" \
-  publish \
-  "project config" \
-  "set scalaVersion := \"2.9.1\"" \
-  "set scalaBinaryVersion := \"2.9.1\"" \
-  publish \
-  "project play-plugin" \
-  "set scalaVersion := \"2.9.1\"" \
-  "set scalaBinaryVersion := \"2.9.1\"" \
-  publish \
-  "project test" \
-  "set scalaVersion := \"2.9.1\"" \
-  "set scalaBinaryVersion := \"2.9.1\"" \
-  publish 
+  "project library" ++2.9.1 publish \
+  "project config" ++2.9.1 publish \
+  "project play-plugin" ++2.9.1 publish \
+  "project test" ++2.9.1 publish 
 
