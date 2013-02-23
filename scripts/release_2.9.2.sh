e57446d7efc52625e736d4d1e4c8ada0141a9c9b
Kazuhiro Sera <seratch@gmail.com>
2013-02-23 13:31:48 +0900

    Updated release script

--- scripts/release_2.9.2.sh
+++ scripts/release_2.9.2.sh
@@ -3,20 +3,9 @@
 cd `dirname $0`/..
 
 sbt \
-  "project library" \
-  "set scalaVersion := \"2.9.2\"" \
-  "set scalaBinaryVersion := \"2.9.2\"" \
-  publish \
-  "project config" \
-  "set scalaVersion := \"2.9.2\"" \
-  "set scalaBinaryVersion := \"2.9.2\"" \
-  publish \
-  "project test" \
-  "set scalaVersion := \"2.9.2\"" \
-  "set scalaBinaryVersion := \"2.9.2\"" \
-  publish \
-  "project mapper-generator" \
-  "set scalaVersion := \"2.9.2\"" \
-  "set scalaBinaryVersion := \"2.9.2\"" \
-  publish 
+  "project library" ++2.9.2 publish \
+  "project config" ++2.9.2 publish \
+  "project test" ++2.9.2 publish \
+  "project mapper-generator" ++2.9.2 publish \
+  "project library" ++2.9.2 publish 
 
