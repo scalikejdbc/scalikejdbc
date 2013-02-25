1715631db295f1241e5ccbd8c86b3b7713de8b04
Kazuhiro Sera <seratch@gmail.com>
2013-02-25 20:35:49 +0900

    Added sub-query support

--- scalikejdbc-interpolation/src/main/scala/scalikejdbc/SQLInterpolation.scala
+++ scalikejdbc-interpolation/src/main/scala/scalikejdbc/SQLInterpolation.scala
@@ -27,7 +27,7 @@ object SQLInterpolation {
     def columns: Seq[String]
 
     def forceUpperCase: Boolean = false
-    def delimiterForResultName = if (forceUpperCase) "__ON__" else "__on__"
+    def delimiterForResultName = if (forceUpperCase) "__ON_" else "__on_"
     def nameConverters: Map[String, String] = Map()
 
     def syntax = {
@@ -65,6 +65,7 @@ object SQLInterpolation {
       }
       c(columnName)
     }
+
     def selectDynamic(name: String): SQLSyntax = field(name)
   }
 
@@ -100,6 +101,13 @@ object SQLInterpolation {
     def forceUpperCase = support.forceUpperCase
     def delimiterForResultName = support.delimiterForResultName
     def columns: Seq[SQLSyntax] = support.columns.map { c => if (support.forceUpperCase) c.toUpperCase else c }.map(c => SQLSyntax(c))
+
+    def notFoundInColumns(name: String): IllegalArgumentException = notFoundInColumns(name, columns.map(_.value).mkString(","))
+
+    def notFoundInColumns(name: String, registeredNames: String): IllegalArgumentException = {
+      new IllegalArgumentException(ErrorMessage.INVALID_COLUMN_NAME + s" (name: ${name}, registered names: ${registeredNames})")
+    }
+
   }
 
   /**
@@ -117,11 +125,12 @@ object SQLInterpolation {
 
     def * : SQLSyntax = SQLSyntax(columns.map(c => s"${tableAliasName}.${c.value}").mkString(", "))
 
-    def column(name: String): SQLSyntax = columns.find(_.value == name).map { c =>
+    def column(name: String): SQLSyntax = columns.find(_.value.toLowerCase == name.toLowerCase).map { c =>
       SQLSyntax(s"${tableAliasName}.${c.value}")
     }.getOrElse {
-      throw new IllegalArgumentException(ErrorMessage.INVALID_COLUMN_NAME + " (" + name + ")")
+      throw new IllegalArgumentException(ErrorMessage.INVALID_COLUMN_NAME + s" (name: ${name}, registered names: ${columns.map(_.value).mkString(",")})")
     }
+
   }
 
   /**
@@ -136,11 +145,9 @@ object SQLInterpolation {
       s"${tableAliasName}.${c.value} as ${c.value}${delimiterForResultName}${tableAliasName}"
     }.mkString(", "))
 
-    def column(name: String): SQLSyntax = columns.find(_.value == name).map { c =>
+    def column(name: String): SQLSyntax = columns.find(_.value.toLowerCase == name.toLowerCase).map { c =>
       SQLSyntax(s"${tableAliasName}.${c.value} as ${c.value}${delimiterForResultName}${tableAliasName}")
-    }.getOrElse {
-      throw new IllegalArgumentException(ErrorMessage.INVALID_COLUMN_NAME + " (" + name + ")")
-    }
+    }.getOrElse(throw notFoundInColumns(name))
   }
 
   /**
@@ -149,14 +156,202 @@ object SQLInterpolation {
   case class ResultNameSQLSyntaxProvider[S <: SQLSyntaxSupport[A], A](support: S, tableAliasName: String)
       extends SQLSyntaxProviderCommonImpl[S, A](support, tableAliasName) {
 
-    def column(name: String): SQLSyntax = columns.find(_.value == name).map { c =>
-      SQLSyntax(s"${c.value}${delimiterForResultName}${tableAliasName}")
-    }.getOrElse {
-      throw new IllegalArgumentException(ErrorMessage.INVALID_COLUMN_NAME + " (" + name + ")")
+    def * : SQLSyntax = SQLSyntax(columns.map { c =>
+      s"${c.value}${delimiterForResultName}${tableAliasName}"
+    }.mkString(", "))
+
+    def namedColumns: Seq[SQLSyntax] = support.columns.map { columnName: String =>
+      SQLSyntax(s"${columnName}${delimiterForResultName}${tableAliasName}")
     }
+
+    def namedColumn(name: String) = namedColumns.find(_.value.toLowerCase == name.toLowerCase).getOrElse {
+      throw new IllegalArgumentException(ErrorMessage.INVALID_COLUMN_NAME + s" (name: ${name}, registered names: ${namedColumns.map(_.value).mkString(",")})")
+    }
+
+    def column(name: String): SQLSyntax = columns.find(_.value.toLowerCase == name.toLowerCase).map { c =>
+      SQLSyntax(s"${c.value}${delimiterForResultName}${tableAliasName}")
+    }.getOrElse(throw notFoundInColumns(name))
+  }
+
+  // --------------------
+  // subquery syntax providers
+  // --------------------
+
+  object SubQuery {
+
+    def syntax(name: String, resultNames: ResultNameSQLSyntaxProvider[_, _]*) = {
+      SubQuerySQLSyntaxProvider(name, resultNames.head.delimiterForResultName, resultNames)
+    }
+
+    def syntax(name: String, delimiterForResultName: String, resultNames: ResultNameSQLSyntaxProvider[_, _]*) = {
+      SubQuerySQLSyntaxProvider(name, delimiterForResultName, resultNames)
+    }
+
+    def as(subquery: SubQuerySQLSyntaxProvider): SQLSyntax = SQLSyntax(subquery.aliasName)
+  }
+
+  case class SubQuerySQLSyntaxProvider(aliasName: String, delimiterForResultName: String, resultNames: Seq[ResultNameSQLSyntaxProvider[_, _]]) {
+
+    def result: SubQueryResultSQLSyntaxProvider = SubQueryResultSQLSyntaxProvider(aliasName, delimiterForResultName, resultNames)
+
+    def resultName: SubQueryResultNameSQLSyntaxProvider = result.name
+
+    def * : SQLSyntax = SQLSyntax(resultNames.map { resultName =>
+      resultName.namedColumns.map { c => s"${aliasName}.${c.value}" }
+    }.mkString(", "))
+
+    def apply(name: SQLSyntax): SQLSyntax = {
+      resultNames.find(rn => rn.namedColumns.find(_.value.toLowerCase == name.value.toLowerCase).isDefined).map { rn =>
+        SQLSyntax(s"${aliasName}.${rn.namedColumn(name).value}")
+      }.getOrElse {
+        val registeredNames = resultNames.map { rn => rn.columns.map(_.value).mkString(",") }.mkString(",")
+        throw new IllegalArgumentException(ErrorMessage.INVALID_COLUMN_NAME + s" (name: ${name.value}, registered names: ${registeredNames})")
+      }
+    }
+
+    def apply[S <: SQLSyntaxSupport[A], A](syntax: QuerySQLSyntaxProvider[S, A]): PartialSubQuerySQLSyntaxProvider[S, A] = {
+      PartialSubQuerySQLSyntaxProvider(aliasName, delimiterForResultName, syntax.resultName)
+    }
+
+  }
+
+  case class SubQueryResultSQLSyntaxProvider(aliasName: String, delimiterForResultName: String, resultNames: Seq[ResultNameSQLSyntaxProvider[_, _]]) {
+
+    def name: SubQueryResultNameSQLSyntaxProvider = SubQueryResultNameSQLSyntaxProvider(aliasName, delimiterForResultName, resultNames)
+
+    def * : SQLSyntax = SQLSyntax(resultNames.map { rn =>
+      rn.namedColumns.map { c =>
+        s"${aliasName}.${c.value} as ${c.value}${delimiterForResultName}${aliasName}"
+      }.mkString(", ")
+    }.mkString(", "))
+
+    def column(name: String): SQLSyntax = {
+      resultNames.find(rn => rn.namedColumns.find(_.value.toLowerCase == name.toLowerCase).isDefined).map { rn =>
+        SQLSyntax(s"${aliasName}.${rn.column(name)} as ${rn.column(name)}${delimiterForResultName}${aliasName}")
+      }.getOrElse {
+        val registeredNames = resultNames.map { rn => rn.columns.map(_.value).mkString(",") }.mkString(",")
+        throw new IllegalArgumentException(ErrorMessage.INVALID_COLUMN_NAME + s" (name: ${name}, registered names: ${registeredNames})")
+      }
+    }
+
+  }
+
+  case class SubQueryResultNameSQLSyntaxProvider(aliasName: String, delimiterForResultName: String, resultNames: Seq[ResultNameSQLSyntaxProvider[_, _]]) {
+
+    def * : SQLSyntax = SQLSyntax(resultNames.map { rn =>
+      rn.namedColumns.map { c =>
+        s"${c.value}${delimiterForResultName}${aliasName}"
+      }.mkString(", ")
+    }.mkString(", "))
+
+    def columns: Seq[SQLSyntax] = resultNames.flatMap { rn =>
+      rn.namedColumns.map { c => SQLSyntax(s"${c.value}${delimiterForResultName}${aliasName}") }
+    }
+
+    def column(name: String): SQLSyntax = columns.find(_.value.toLowerCase == name.toLowerCase).getOrElse {
+      throw notFoundInColumns(name)
+    }
+
+    def apply(name: SQLSyntax): SQLSyntax = {
+      resultNames.find(rn => rn.namedColumns.find(_.value.toLowerCase == name.toLowerCase).isDefined).map { rn =>
+        SQLSyntax(s"${rn.namedColumn(name).value}${delimiterForResultName}${aliasName}")
+      }.getOrElse {
+        throw notFoundInColumns(name.value)
+      }
+    }
+
+    def notFoundInColumns(name: String) = {
+      val registeredNames = resultNames.map { rn => rn.namedColumns.map(_.value).mkString(",") }.mkString(",")
+      new IllegalArgumentException(ErrorMessage.INVALID_COLUMN_NAME + s" (name: ${name}, registered names: ${registeredNames})")
+    }
+
+  }
+
+  // --------------------
+  // partial subquery syntax providers
+  // --------------------
+
+  case class PartialSubQuerySQLSyntaxProvider[S <: SQLSyntaxSupport[A], A](aliasName: String, override val delimiterForResultName: String, underlying: ResultNameSQLSyntaxProvider[S, A])
+      extends SQLSyntaxProviderCommonImpl[S, A](underlying.support, aliasName) {
+
+    def result: PartialSubQueryResultSQLSyntaxProvider[S, A] = {
+      PartialSubQueryResultSQLSyntaxProvider(aliasName, delimiterForResultName, underlying)
+    }
+
+    def resultName: PartialSubQueryResultNameSQLSyntaxProvider[S, A] = result.name
+
+    def * : SQLSyntax = SQLSyntax(resultName.namedColumns.map { c => s"${aliasName}.${c.value}" }.mkString(", "))
+
+    def apply(name: SQLSyntax): SQLSyntax = {
+      underlying.namedColumns.find(_.value.toLowerCase == name.toLowerCase).map { _ =>
+        SQLSyntax(s"${aliasName}.${underlying.namedColumn(name).value}")
+      }.getOrElse {
+        throw notFoundInColumns(name.value, resultName.columns.map(_.value).mkString(","))
+      }
+    }
+
+    def column(name: String) = {
+      SQLSyntax(s"${aliasName}.${underlying.column(name).value}")
+    }
+
+  }
+
+  case class PartialSubQueryResultSQLSyntaxProvider[S <: SQLSyntaxSupport[A], A](aliasName: String, override val delimiterForResultName: String, underlying: ResultNameSQLSyntaxProvider[S, A])
+      extends SQLSyntaxProviderCommonImpl[S, A](underlying.support, aliasName) {
+
+    def name: PartialSubQueryResultNameSQLSyntaxProvider[S, A] = {
+      PartialSubQueryResultNameSQLSyntaxProvider(aliasName, delimiterForResultName, underlying)
+    }
+
+    def * : SQLSyntax = SQLSyntax(underlying.namedColumns.map { c =>
+      s"${aliasName}.${c.value} as ${c.value}${delimiterForResultName}${aliasName}"
+    }.mkString(", "))
+
+    def column(name: String): SQLSyntax = {
+      underlying.namedColumns.find(_.value.toLowerCase == name.toLowerCase).map { nc =>
+        SQLSyntax(s"${aliasName}.${nc.value} as ${nc.value}${delimiterForResultName}${aliasName}")
+      }.getOrElse {
+        throw notFoundInColumns(name, underlying.columns.map(_.value).mkString(","))
+      }
+    }
+
+  }
+
+  case class PartialSubQueryResultNameSQLSyntaxProvider[S <: SQLSyntaxSupport[A], A](aliasName: String, override val delimiterForResultName: String, underlying: ResultNameSQLSyntaxProvider[S, A])
+      extends SQLSyntaxProviderCommonImpl[S, A](underlying.support, aliasName) {
+
+    def * : SQLSyntax = SQLSyntax(underlying.namedColumns.map { c =>
+      s"${c.value}${delimiterForResultName}${aliasName}"
+    }.mkString(", "))
+
+    override def columns: Seq[SQLSyntax] = underlying.namedColumns.map { c => SQLSyntax(s"${c.value}${delimiterForResultName}${aliasName}") }
+
+    def column(name: String): SQLSyntax = underlying.columns.find(_.value.toLowerCase == name.toLowerCase).map { original: SQLSyntax =>
+      SQLSyntax(s"${original.value}${delimiterForResultName}${underlying.tableAliasName}${delimiterForResultName}${aliasName}")
+    }.getOrElse {
+      throw notFoundInColumns(name, underlying.columns.map(_.value).mkString(","))
+    }
+
+    def namedColumns: Seq[SQLSyntax] = underlying.namedColumns.map { nc: SQLSyntax =>
+      SQLSyntax(s"${nc.value}${delimiterForResultName}${aliasName}")
+    }
+
+    def namedColumn(name: String) = underlying.namedColumns.find(_.value.toLowerCase == name.toLowerCase).getOrElse {
+      throw notFoundInColumns(name, namedColumns.map(_.value).mkString(","))
+    }
+
+    def apply(name: SQLSyntax): SQLSyntax = {
+      underlying.namedColumns.find(_.value.toLowerCase == name.toLowerCase).map { nc =>
+        SQLSyntax(s"${nc.value}${delimiterForResultName}${aliasName}")
+      }.getOrElse {
+        throw notFoundInColumns(name.value, underlying.columns.map(_.value).mkString(","))
+      }
+    }
+
   }
 
   type ResultName[A] = ResultNameSQLSyntaxProvider[SQLSyntaxSupport[A], A]
+  type SubQueryResultName = SubQueryResultNameSQLSyntaxProvider
 
   @inline implicit def convertSQLSyntaxToString(syntax: SQLSyntax): String = syntax.value
   @inline implicit def interpolation(s: StringContext) = new SQLInterpolation(s)
