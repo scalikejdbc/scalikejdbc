86da5c70095d11c63acd6342b3e724d76cab8ee5
Kazuhiro Sera <seratch@gmail.com>
2013-03-20 03:42:45 +0900

    Added TODO

--- scalikejdbc-interpolation-macro/src/main/scala/scalikejdbc/interpolation/SQLSyntaxFieldMacro.scala
+++ scalikejdbc-interpolation-macro/src/main/scala/scalikejdbc/interpolation/SQLSyntaxFieldMacro.scala
@@ -9,6 +9,7 @@ object SQLSyntaxFieldMacro {
 
   def validateImpl(c: Context)(name: c.Expr[String], expectedNames: c.Expr[Seq[String]]): c.Expr[String] = {
     import c.universe._
+    // TODO eval doesn't work ...
     val _name: String = c.eval(name)
     val _expectedNames: Seq[String] = c.eval(expectedNames)
     if (!_expectedNames.contains(_name)) {
