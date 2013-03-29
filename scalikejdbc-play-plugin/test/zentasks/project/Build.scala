66d5928363c6f6811bd6698c441b48f5c1d5c1f7
Kazuhiro Sera <seratch@gmail.com>
2013-03-29 22:46:48 +0900

    Bumped scalikejdbc version

--- scalikejdbc-play-plugin/test/zentasks/project/Build.scala
+++ scalikejdbc-play-plugin/test/zentasks/project/Build.scala
@@ -9,12 +9,11 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
-      "org.scala-lang"     %  "scala-library"             % "2.10.0",
       "com.h2database"     %  "h2"                        % "[1.3,)",
       "postgresql"         %  "postgresql"                % "9.1-901.jdbc4",
-      "com.github.seratch" %% "scalikejdbc"               % "1.5.0",
-      "com.github.seratch" %% "scalikejdbc-interpolation" % "1.5.0",
-      "com.github.seratch" %% "scalikejdbc-play-plugin"   % "1.5.0"
+      "com.github.seratch" %% "scalikejdbc"               % "1.5.1",
+      "com.github.seratch" %% "scalikejdbc-interpolation" % "1.5.1",
+      "com.github.seratch" %% "scalikejdbc-play-plugin"   % "1.5.1"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
