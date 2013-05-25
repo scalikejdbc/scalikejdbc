c6c529781d60b38b02720ee216a8952608971c83
Kazuhiro Sera <seratch@gmail.com>
2013-05-25 12:08:27 +0900

    Improved test runners

--- scripts/run_interpolation_tests.sh
+++ scripts/run_interpolation_tests.sh
@@ -7,12 +7,15 @@ cd `dirname $0`
 cd ..
 cp -p scalikejdbc-library/src/test/resources/jdbc_$1.properties scalikejdbc-library/src/test/resources/jdbc.properties
 
-sbt ++2.10.1 clean                       > logs/test_stdout.log
+sbt ++2.10.1 clean                        > logs/test_stdout.log
 sbt ++2.10.1 interpolation-core/test     >> logs/test_stdout.log
 sbt ++2.10.1 interpolation-macro/compile >> logs/test_stdout.log
 sbt ++2.10.1 interpolation/test          >> logs/test_stdout.log
 
 cp -p scalikejdbc-library/src/test/resources/jdbc_hsqldb.properties scalikejdbc-library/src/test/resources/jdbc.properties
 
-grep -A 5 "Failed tests:" logs/test_stdout.log
+grep -A 5 "FAILED"              logs/test_stdout.log
+grep -A 5 "Error during tests:" logs/test_stdout.log
+grep -A 5 "Failed tests:"       logs/test_stdout.log
+
 
