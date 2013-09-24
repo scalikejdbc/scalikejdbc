e63c621fc33c63cb9c03dc7b03db3b53b2b25477
Kazuhiro Sera <seratch@gmail.com>
2013-09-25 08:39:58 +0900

    version 1.6.9

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object ScalikeJDBCProjects extends Build {
 
   // [NOTE] Execute the following to bump version
   // sbt "g version 1.3.8-SNAPSHOT"
-  lazy val _version = "1.6.9-SNAPSHOT"
+  lazy val _version = "1.6.9"
 
   // published dependency version
   lazy val _slf4jApiVersion = "1.7.5"
