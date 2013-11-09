82a0fc62a0e47d5c366231031836351a23a504b3
Kazuhiro Sera <seratch@gmail.com>
2013-11-09 09:35:18 +0900

    Added tests for pull request #170

--- scalikejdbc-library/src/test/scala/scalikejdbc/TypeBinderSpec.scala
+++ scalikejdbc-library/src/test/scala/scalikejdbc/TypeBinderSpec.scala
@@ -4,6 +4,7 @@ import org.scalatest._
 import mock.MockitoSugar
 import org.mockito.Mockito._
 import org.scalatest.matchers._
+import java.sql.ResultSet
 
 class TypeBinderSpec extends FlatSpec with ShouldMatchers with MockitoSugar {
 
@@ -12,6 +13,23 @@ class TypeBinderSpec extends FlatSpec with ShouldMatchers with MockitoSugar {
   it should "be able to return an Option value when NPE" in {
     val result: Option[String] = implicitly[TypeBinder[Option[String]]].apply(null, 1)
     result should be(None)
+  }
+
+  it should "fix issue #170" in {
+    val rs: ResultSet = mock[ResultSet]
+    when(rs.getObject("none")).thenReturn(null, Array[Object](): _*)
+
+    // WrappedResultSet works fine
+    val wrapped = WrappedResultSet(rs, new ResultSetCursor(0), 0)
+    wrapped.booleanOpt("none") should be(None)
+    wrapped.intOpt("none") should be(None)
+    wrapped.longOpt("none") should be(None)
+
+    // using TypeBinder implicitly doesn't work as expected
+    implicitly[TypeBinder[Option[Boolean]]].apply(rs, "none") should be(None)
+    implicitly[TypeBinder[Option[Int]]].apply(rs, "none") should be(None)
+    implicitly[TypeBinder[Option[Long]]].apply(rs, "none") should be(None)
+    implicitly[TypeBinder[Option[String]]].apply(rs, "none") should be(None)
   }
 
 }
