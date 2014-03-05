4f42f793adb10689c452e43356ae394303aaaed7
Kazuhiro Sera <seratch@gmail.com>
2014-03-05 22:54:02 +0900

    Added Travis CI settings again

--- travis.sh
+++ travis.sh
@@ -2,9 +2,13 @@
 rm -f ./sbt
 wget https://raw.github.com/paulp/sbt-extras/master/sbt &&
 chmod u+x ./sbt &&
-./sbt -sbt-version 0.12.2 \
+./sbt -sbt-version 0.13.2-M2 \
+  ++2.10.3 \
   clean \
-  ++2.9.2  test \
-  ++2.10.0 test \
-  ++2.10.0 interpolation/test
+  interpolation-core/test \
+  interpolation/test \
+  config/test \
+  test/test \
+  play-plugin/test \
+  play-fixture-plugin/test
 
