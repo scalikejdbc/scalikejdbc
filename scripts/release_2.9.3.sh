c28c4f687297608c28e8441b34d29905a0b79cba
xuwei-k <6b656e6a69@gmail.com>
2014-03-10 00:57:53 +0900

    remove unnecessary scripts

--- scripts/release_2.9.3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd `dirname $0`/..
-sbt "project library" 'set scalaVersion := "2.9.3"' 'set scalaBinaryVersion := "2.9.3"' publishSigned \
-  "project config" 'set scalaVersion := "2.9.3"' 'set scalaBinaryVersion := "2.9.3"' publishSigned \
-  "project test" 'set scalaVersion := "2.9.3"' 'set scalaBinaryVersion := "2.9.3"' publishSigned \
-  "project mapper-generator-core" 'set scalaVersion := "2.9.3"' 'set scalaBinaryVersion := "2.9.3"' publishSigned 
-
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/bin/sh
 if [ $# -ne 1 ]; then
   echo "Please specify db name (i.e. postgresql)"
   exit 1
@@ -8,9 +8,6 @@ cd ..
 cp -p scalikejdbc-library/src/test/resources/jdbc_$1.properties scalikejdbc-library/src/test/resources/jdbc.properties
 
 sbt \
-  ++2.9.2 \
-  clean \
-  library/test \
   ++2.10.3 \
   clean \
   interpolation-core/test \
