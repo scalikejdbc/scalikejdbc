3d91f39b1fa6dee555b11c22ef12d8f5eafa47a7
Kazuhiro Sera <seratch@gmail.com>
2014-05-06 13:51:10 +0900

    Added ParameterBinder to enable customizing StatementExecutor#binParams

---
+++ scalikejdbc-core/src/main/scala/scalikejdbc/ParameterBinder.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2014 scalikejdbc.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package scalikejdbc
+
+import java.sql.PreparedStatement
+
+/**
+ * ParameterBinder which enables customizing StatementExecutor#binParams.
+ *
+ * {{{
+ * val bytes = Array[Byte](1,2,3, ...)
+ * val in = ByteArrayInputStream(bytes)
+ * val bin = ParameterBinder(
+ *   value = in,
+ *   binder = (stmt, idx) => stmt.setBinaryStream(idx, in, bytes.length)
+ * )
+ * sql"insert into table (bin) values (${bin})".update.apply()
+ * }}}
+ */
+trait ParameterBinder[A] {
+
+  /**
+   * Parameter value.
+   */
+  def value: A
+
+  /**
+   * Applies parameter to PreparedStatement.
+   */
+  def apply(stmt: PreparedStatement, idx: Int): Unit
+
+}
+
+/**
+ * ParameterBinder factory.
+ */
+object ParameterBinder {
+
+  /**
+   * Factory method for ParameterBinder.
+   */
+  def apply[A](value: A, binder: (PreparedStatement, Int) => Unit): ParameterBinder[A] = {
+    val _v = value
+    new ParameterBinder[A] {
+      override def value: A = _v
+      override def apply(stmt: PreparedStatement, idx: Int): Unit = binder.apply(stmt, idx)
+    }
+  }
+
+}
+
