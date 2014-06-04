05e77d4dd0a1597554e98c3d35f21c9ce52f8203
Kazuhiro Sera <seratch@gmail.com>
2014-06-04 13:40:44 +0900

    2.0.2-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object ScalikeJDBCProjects extends Build {
 
   // [NOTE] Execute the following to bump version
   // sbt "g version 1.3.8-SNAPSHOT"
-  lazy val _version = "2.0.1"
+  lazy val _version = "2.0.2-SNAPSHOT"
   lazy val compatibleVersion = "2.0.0"
 
   lazy val _organization = "org.scalikejdbc"
