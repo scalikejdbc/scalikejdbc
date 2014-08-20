e94b34e80108f85f8223189f01aae58d8b14573d
Kazuhiro Sera <seratch@gmail.com>
2014-08-20 12:46:24 +0900

    version 2.1.0.RC1

--- scripts/release_2.10.sh
+++ scripts/release_2.10.sh
@@ -12,6 +12,7 @@ sbt 'set scalaVersion := "2.10.4"' \
   "project interpolation" 'set scalaVersion := "2.10.4"' publishSigned \
   "project library" 'set scalaVersion := "2.10.4"' publishSigned \
   "project jsr310" 'set scalaVersion := "2.10.4"' publishSigned \
+  "project syntax-support-macro" 'set scalaVersion := "2.10.4"' publishSigned \
   "project mapper-generator-core" 'set scalaVersion := "2.10.4"' publishSigned \
   "project test" 'set scalaVersion := "2.10.4"' publishSigned 
 
--- scripts/release_2.11.sh
+++ scripts/release_2.11.sh
@@ -12,6 +12,7 @@ sbt 'set scalaVersion := "2.11.2"' \
   "project interpolation" 'set scalaVersion := "2.11.2"' publishSigned \
   "project library" 'set scalaVersion := "2.11.2"' publishSigned \
   "project jsr310" 'set scalaVersion := "2.11.2"' publishSigned \
+  "project syntax-support-macro" 'set scalaVersion := "2.11.2"' publishSigned \
   "project test" 'set scalaVersion := "2.11.2"' publishSigned \
   "project mapper-generator-core" 'set scalaVersion := "2.11.2"' publishSigned 
 
