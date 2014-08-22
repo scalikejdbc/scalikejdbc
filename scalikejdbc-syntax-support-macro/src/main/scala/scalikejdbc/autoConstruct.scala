8c8758dc2179ce9046ccb7b89ea451ff685dd9d0
xuwei-k <6b656e6a69@gmail.com>
2014-08-23 03:19:40 +0900

    add autoConstruct#debug

--- scalikejdbc-syntax-support-macro/src/main/scala/scalikejdbc/autoConstruct.scala
+++ scalikejdbc-syntax-support-macro/src/main/scala/scalikejdbc/autoConstruct.scala
@@ -21,6 +21,18 @@ object autoConstruct {
     applyResultName_impl(c)(rs, c.Expr[ResultName[A]](q"${sp}.resultName"), excludes:_*)
   }
 
+  def applyResultNameDebug[A: c.WeakTypeTag](c: Context)(rs: c.Expr[WrappedResultSet], rn: c.Expr[ResultName[A]], excludes: c.Expr[String]*): c.Expr[A] = {
+    val expr = applyResultName_impl[A](c)(rs, rn, excludes: _*)
+    println(expr.tree)
+    expr
+  }
+
+  def applySyntaxProviderDebug[A: c.WeakTypeTag](c: Context)(rs: c.Expr[WrappedResultSet], sp: c.Expr[SyntaxProvider[A]], excludes: c.Expr[String]*): c.Expr[A] = {
+    val expr = applySyntaxProvider_impl[A](c)(rs, sp, excludes: _*)
+    println(expr.tree)
+    expr
+  }
+
   private[this] def constructorParams[A: c.WeakTypeTag](c: Context)(excludes: c.Expr[String]*) = {
     import c.universe._
     val declarations = decls(c)(weakTypeTag[A].tpe)
@@ -43,5 +55,9 @@ object autoConstruct {
   def apply[A](rs: WrappedResultSet, rn: ResultName[A], excludes: String*): A = macro applyResultName_impl[A]
 
   def apply[A](rs: WrappedResultSet, sp: SyntaxProvider[A], excludes: String*): A = macro applySyntaxProvider_impl[A]
+
+  def debug[A](rs: WrappedResultSet, rn: ResultName[A], excludes: String*): A = macro applyResultNameDebug[A]
+
+  def debug[A](rs: WrappedResultSet, sp: SyntaxProvider[A], excludes: String*): A = macro applySyntaxProviderDebug[A]
 
 }
