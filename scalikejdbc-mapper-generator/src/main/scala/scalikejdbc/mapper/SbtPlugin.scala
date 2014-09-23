a0300436848890b93444bf016ed3a9ee4b892bfa
xuwei-k <6b656e6a69@gmail.com>
2014-09-23 17:34:21 +0900

    fix issue 285. don't repeat default values

--- scalikejdbc-mapper-generator/src/main/scala/scalikejdbc/mapper/SbtPlugin.scala
+++ scalikejdbc-mapper-generator/src/main/scala/scalikejdbc/mapper/SbtPlugin.scala
@@ -44,6 +44,7 @@ object SbtPlugin extends Plugin {
         inputStream => props.load(inputStream)
       }
     }
+    val defaultConfig = GeneratorConfig()
     (JDBCSettings(
       driver = Option(props.get("jdbc.driver")).map(_.toString).getOrElse(throw new IllegalStateException("Add jdbc.driver to project/scalikejdbc-mapper-generator.properties")),
       url = Option(props.get("jdbc.url")).map(_.toString).getOrElse(throw new IllegalStateException("Add jdbc.url to project/scalikejdbc-mapper-generator.properties")),
@@ -51,14 +52,14 @@ object SbtPlugin extends Plugin {
       password = Option(props.get("jdbc.password")).map(_.toString).getOrElse(""),
       schema = Option(props.get("jdbc.schema")).map(_.toString).orNull[String]
     ), GeneratorSettings(
-        packageName = Option(props.get("generator.packageName")).map(_.toString).getOrElse("models"),
-        template = Option(props.get("generator.template")).map(_.toString).getOrElse("executableSQL"),
-        testTemplate = Option(props.get("generator.testTemplate")).map(_.toString).getOrElse("specs2unit"),
-        lineBreak = Option(props.get("generator.lineBreak")).map(_.toString).getOrElse("LF"),
-        caseClassOnly = Option(props.get("generator.caseClassOnly")).map(_.toString.toBoolean).getOrElse(false),
-        encoding = Option(props.get("generator.encoding")).map(_.toString).getOrElse("UTF-8"),
-        autoConstruct = Option(props.get("generator.autoConstruct")).map(_.toString.toBoolean).getOrElse(false),
-        defaultAutoSession = Option(props.get("generator.defaultAutoSession")).map(_.toString.toBoolean).getOrElse(true)
+        packageName = Option(props.get("generator.packageName")).map(_.toString).getOrElse(defaultConfig.packageName),
+        template = Option(props.get("generator.template")).map(_.toString).getOrElse(defaultConfig.template.name),
+        testTemplate = Option(props.get("generator.testTemplate")).map(_.toString).getOrElse(GeneratorTestTemplate.specs2unit.name),
+        lineBreak = Option(props.get("generator.lineBreak")).map(_.toString).getOrElse(defaultConfig.lineBreak.name),
+        caseClassOnly = Option(props.get("generator.caseClassOnly")).map(_.toString.toBoolean).getOrElse(defaultConfig.caseClassOnly),
+        encoding = Option(props.get("generator.encoding")).map(_.toString).getOrElse(defaultConfig.encoding),
+        autoConstruct = Option(props.get("generator.autoConstruct")).map(_.toString.toBoolean).getOrElse(defaultConfig.autoConstruct),
+        defaultAutoSession = Option(props.get("generator.defaultAutoSession")).map(_.toString.toBoolean).getOrElse(defaultConfig.defaultAutoSession)
       ))
   }
 
