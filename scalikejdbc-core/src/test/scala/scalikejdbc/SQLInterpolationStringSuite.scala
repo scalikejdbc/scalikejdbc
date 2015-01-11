b13c0b0119a4af78287d098dcf7a9084b97b315e
ponkotuy <web@ponkotuy.com>
2015-01-11 11:27:34 +0900

    Add SQLInterpolationString test

---
+++ scalikejdbc-core/src/test/scala/scalikejdbc/SQLInterpolationStringSuite.scala
@@ -0,0 +1,17 @@
+package scalikejdbc
+
+import org.scalatest._
+
+class SQLInterpolationStringSuite extends FlatSpec with Matchers {
+
+  import scalikejdbc.interpolation.Implicits._
+
+  behavior of "SQLInterpolationString"
+
+  it should "be equalization of changing parameter among Set" in {
+    import scala.collection.mutable
+    val s1 = sqls"s in (${Set(1, 2, 3)})"
+    val s2 = sqls"s in (${mutable.Set(1, 2, 3)}"
+    s1.parameters should equal(s2.parameters)
+  }
+}
