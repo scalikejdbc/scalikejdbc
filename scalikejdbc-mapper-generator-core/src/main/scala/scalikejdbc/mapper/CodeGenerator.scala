9cf14ba3a47e9ca3539fe4e58238fc274f3a6040
xuwei-k <6b656e6a69@gmail.com>
2015-02-24 13:50:57 +0900

    add new keys for mapper-generator plugin. be more customizable

--- scalikejdbc-mapper-generator-core/src/main/scala/scalikejdbc/mapper/CodeGenerator.scala
+++ scalikejdbc-mapper-generator-core/src/main/scala/scalikejdbc/mapper/CodeGenerator.scala
@@ -23,7 +23,7 @@ import java.util.Locale.{ ENGLISH => en }
  * Active Record like template generator
  */
 class CodeGenerator(table: Table, specifiedClassName: Option[String] = None)(implicit config: GeneratorConfig = GeneratorConfig())
-    extends LoanPattern {
+    extends Generator with LoanPattern {
 
   import java.sql.{ Types => JavaSqlTypes }
   import java.io.{ OutputStreamWriter, FileOutputStream, File }
