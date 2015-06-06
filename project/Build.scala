6cc7b79a46c7ba7b4c0d68dae39747f5bc7ad5e0
Kazuhiro Sera <seratch@gmail.com>
2015-06-06 11:46:28 +0900

    version 2.2.7

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import sbtbuildinfo.Plugin._
 
 object ScalikeJDBCProjects extends Build {
 
-  lazy val _version = "2.2.7-SNAPSHOT"
+  lazy val _version = "2.2.7"
 
   lazy val _organization = "org.scalikejdbc"
 
