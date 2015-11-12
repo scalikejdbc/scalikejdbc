07d6ae740190713aa26692c813e101de77b11e5f
Kazuhiro Sera <seratch@gmail.com>
2015-11-12 11:48:45 +0900

    version 2.3.0

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -2,16 +2,34 @@ import sbt._, Keys._
 import com.typesafe.tools.mima.plugin.MimaPlugin
 import com.typesafe.tools.mima.plugin.MimaKeys.{previousArtifact, reportBinaryIssues, binaryIssueFilters}
 
+/*
+ * MiMa settings of ScalikeJDBC libs.
+ *
+ * Since 2.3.x series, we don't exclude bin incompatibility after 2.x.0 release.
+ *
+ * see also: https://github.com/scalikejdbc/scalikejdbc/blob/master/CONTRIBUTING.md
+ */
 object MimaSettings {
 
+  // TODO: after releasing 2.3.0, the following settings must be enabled.
   val mimaSettings = Seq.empty
-  // NOTICE: Since 2.3.x series, we don't exclude bin incompatibility after 2.x.0 release.
   /*
+  // The `previousVersion` must be exactly the previous version (e.g. "2.3.1" for "2.3.2-SNAPSHOT").
+  //
+  // The following bad scenario is the reason we must obey the rule:
+  //
+  //  - your build is toward 2.3.2 release and the `previousVersion` is "2.3.0"
+  //  - you've added new methods since 2.3.1
+  //  - you're going to remove some of the methods in 2.3.2
+  //  - MiMa cannot detect such bin-incompatibility
+  //
+  val previousVersion = "2.3.0"
+
   val mimaSettings = MimaPlugin.mimaDefaultSettings ++ Seq(
     previousArtifact := {
       CrossVersion.partialVersion(scalaVersion.value) match {
         case Some((2, scalaMajor)) if scalaMajor <= 11 =>
-          Some(organization.value % s"${name.value}_${scalaBinaryVersion.value}" % "2.2.0")
+          Some(organization.value % s"${name.value}_${scalaBinaryVersion.value}" % previousVersion)
         case _ =>
           None
       }
