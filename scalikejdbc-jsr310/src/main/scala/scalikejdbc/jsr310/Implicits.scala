2f0b7e73da09f8c5a789645085ec5f9858094f58
xuwei-k <6b656e6a69@gmail.com>
2015-11-17 19:13:24 +0900

    remove unused imports

--- scalikejdbc-jsr310/src/main/scala/scalikejdbc/jsr310/Implicits.scala
+++ scalikejdbc-jsr310/src/main/scala/scalikejdbc/jsr310/Implicits.scala
@@ -1,7 +1,5 @@
 package scalikejdbc.jsr310
 
-import java.util.{ GregorianCalendar, Calendar }
-
 import scala.language.implicitConversions
 import java.time._
 import scalikejdbc.{ TypeBinder, WrappedResultSet }
