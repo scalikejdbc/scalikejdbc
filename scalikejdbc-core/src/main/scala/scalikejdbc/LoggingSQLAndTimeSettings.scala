479e8d4bdbcfe289f38b38765d31e2d09e5bdf2f
zaneli <shun.otani@gmail.com>
2016-10-28 20:25:34 +0900

    add LoggingSQLAndTimeSettings.maxColumnSize, maxBatchParamSize

--- scalikejdbc-core/src/main/scala/scalikejdbc/LoggingSQLAndTimeSettings.scala
+++ scalikejdbc-core/src/main/scala/scalikejdbc/LoggingSQLAndTimeSettings.scala
@@ -11,6 +11,7 @@ case class LoggingSQLAndTimeSettings(
   logLevel: Symbol = 'debug,
   warningEnabled: Boolean = false,
   warningThresholdMillis: Long = 3000L,
-  warningLogLevel: Symbol = 'warn
+  warningLogLevel: Symbol = 'warn,
+  maxColumnSize: Option[Int] = Some(100),
+  maxBatchParamSize: Option[Int] = Some(20)
 )
-
