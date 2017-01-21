0f29d00d35ddf9209d88dc47bb0bd6245591961b
smdmts <smdmts@gmail.com>
2017-01-21 05:48:48 +0000

    support datasouce closing

--- scalikejdbc-core/src/main/scala/scalikejdbc/DataSourceConnectionPool.scala
+++ scalikejdbc-core/src/main/scala/scalikejdbc/DataSourceConnectionPool.scala
@@ -8,7 +8,8 @@ import java.sql.Connection
  */
 class DataSourceConnectionPool(
   override val dataSource: DataSource,
-  settings: DataSourceConnectionPoolSettings = DataSourceConnectionPoolSettings()
+  settings: DataSourceConnectionPoolSettings,
+  closer: DataSourceCloser
 )
     extends ConnectionPool(
       url = "<external-data-source>",
@@ -17,7 +18,21 @@ class DataSourceConnectionPool(
       settings = ConnectionPoolSettings(driverName = settings.driverName)
     ) {
 
+  def this(dataSource: DataSource) {
+    this(dataSource, DataSourceConnectionPoolSettings(), DefaultDataSourceCloser)
+  }
+
+  def this(dataSource: DataSource, settings: DataSourceConnectionPoolSettings = DataSourceConnectionPoolSettings()) {
+    this(dataSource, settings, DefaultDataSourceCloser)
+  }
+
+  def this(dataSource: DataSource, closer: DataSourceCloser) {
+    this(dataSource, DataSourceConnectionPoolSettings(), closer)
+  }
+
   override def borrow(): Connection = dataSource.getConnection()
+
+  override def close(): Unit = closer.close()
 }
 
 /**
@@ -29,7 +44,8 @@ class AuthenticatedDataSourceConnectionPool(
   override val dataSource: DataSource,
   override val user: String,
   password: String,
-  settings: DataSourceConnectionPoolSettings = DataSourceConnectionPoolSettings()
+  settings: DataSourceConnectionPoolSettings,
+  closer: DataSourceCloser
 )
     extends ConnectionPool(
       url = "<external-data-source>",
@@ -38,5 +54,30 @@ class AuthenticatedDataSourceConnectionPool(
       settings = ConnectionPoolSettings(driverName = settings.driverName)
     ) {
 
+  def this(dataSource: DataSource, user: String, password: String) {
+    this(dataSource, user, password, DataSourceConnectionPoolSettings(), DefaultDataSourceCloser)
+  }
+
+  def this(dataSource: DataSource, user: String, password: String, settings: DataSourceConnectionPoolSettings) {
+    this(dataSource, user, password, settings, DefaultDataSourceCloser)
+  }
+
+  def this(dataSource: DataSource, user: String, password: String, closer: DataSourceCloser) {
+    this(dataSource, user, password, DataSourceConnectionPoolSettings(), closer)
+  }
+
   override def borrow(): Connection = dataSource.getConnection(user, password)
+
+  override def close(): Unit = closer.close()
+}
+
+/**
+ * Closing support for DataSource
+ */
+trait DataSourceCloser {
+  def close(): Unit
+}
+
+private case object DefaultDataSourceCloser extends DataSourceCloser {
+  def close(): Unit = throw new UnsupportedOperationException
 }
