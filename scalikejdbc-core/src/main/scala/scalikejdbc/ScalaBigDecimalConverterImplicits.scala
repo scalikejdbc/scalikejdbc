1b8b29d4aef40abcf5a270e426b2c7abfb3573a3
zettelmj <zettelmeyerj@googlemail.com>
2017-02-02 14:17:56 +0000

    fix class duplication

--- scalikejdbc-core/src/main/scala/scalikejdbc/ScalaBigDecimalConverterImplicits.scala
+++ scalikejdbc-core/src/main/scala/scalikejdbc/ScalaBigDecimalConverterImplicits.scala
@@ -11,17 +11,6 @@ trait ScalaBigDecimalConverterImplicits {
     new ScalaBigDecimalConverter(bd)
   }
 
-}
-
-/**
-  * Implicit conversions for option BigDecimal values.
-  */
-trait ScalaBigDecimalConverterImplicits {
-
-  implicit def convertBigDecimal(bd: java.math.BigDecimal): ScalaBigDecimalConverter = {
-    new ScalaBigDecimalConverter(bd)
-  }
-
   implicit def convertBigDecimalOpt(bd: Option[java.math.BigDecimal]): ScalaBigDecimalConverterOpt = {
     new ScalaBigDecimalConverterOpt(bd)
   }
