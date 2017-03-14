2d3911707f9a19fdd1827517694dad43a6586850
xuwei-k <6b656e6a69@gmail.com>
2017-03-14 16:49:16 +0900

    fix exhaustive warnings. change "sealed abstract class" and case object

--- scalikejdbc-mapper-generator-core/src/main/scala/scalikejdbc/mapper/GeneratorTemplate.scala
+++ scalikejdbc-mapper-generator-core/src/main/scala/scalikejdbc/mapper/GeneratorTemplate.scala
@@ -1,8 +1,15 @@
 package scalikejdbc.mapper
 
-case class GeneratorTemplate(name: String)
+sealed abstract class GeneratorTemplate(val name: String) extends Product with Serializable
 
 object GeneratorTemplate {
-  val interpolation = GeneratorTemplate("interpolation")
-  val queryDsl = GeneratorTemplate("queryDsl")
+  case object interpolation extends GeneratorTemplate("interpolation")
+  case object queryDsl extends GeneratorTemplate("queryDsl")
+
+  private[this] val all = Set(interpolation, queryDsl)
+  private[this] val map: Map[String, GeneratorTemplate] =
+    all.map(t => t.name -> t).toMap
+
+  def apply(name: String): GeneratorTemplate =
+    map.getOrElse(name, sys.error(s"$name is not valid template name"))
 }
