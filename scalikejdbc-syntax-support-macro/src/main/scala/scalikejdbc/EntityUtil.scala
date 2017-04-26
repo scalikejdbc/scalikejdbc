bf1293f8e7547743041b160733cc387e8503fc87
Shunsuke Otani <shun.otani@gmail.com>
2017-04-26 16:53:25 +0900

    use collection.breakOut (#682)

--- scalikejdbc-syntax-support-macro/src/main/scala/scalikejdbc/EntityUtil.scala
+++ scalikejdbc-syntax-support-macro/src/main/scala/scalikejdbc/EntityUtil.scala
@@ -20,8 +20,8 @@ private[scalikejdbc] object EntityUtil {
         c.error(c.enclosingPosition, s"You must use String literal values for field names to exclude from #$macroName's targets. $m could not resolve at compile time.")
         None
       }
-    }.toSet
-    val paramsStrs: Set[String] = allParams.map(_.name.decodedName.toString).toSet
+    }(collection.breakOut)
+    val paramsStrs: Set[String] = allParams.map(_.name.decodedName.toString)(collection.breakOut)
     excludeStrs.foreach { ex =>
       if (!paramsStrs(ex)) c.error(c.enclosingPosition, s"$ex does not found in ${weakTypeTag[A].tpe}")
     }
