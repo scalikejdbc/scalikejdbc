06f910f7232ebe19bcfe3f5c10e92fcb802d184b
Kazuhiro <seratch@gmail.com>
2017-06-13 07:16:45 +0900

    Start development for 3.0.2-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import sbtbuildinfo.BuildInfoPlugin.autoImport._
 
 object ScalikeJDBCProjects extends Build {
 
-  lazy val _version = "3.0.1"
+  lazy val _version = "3.0.2-SNAPSHOT"
 
   lazy val _organization = "org.scalikejdbc"
 
