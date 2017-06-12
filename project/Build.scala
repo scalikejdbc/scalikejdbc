f4b0517e429f6cab6753814bb901b36fcd78b32b
Kazuhiro <seratch@gmail.com>
2017-06-13 07:15:21 +0900

    version 3.0.1

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import sbtbuildinfo.BuildInfoPlugin.autoImport._
 
 object ScalikeJDBCProjects extends Build {
 
-  lazy val _version = "3.0.1-SNAPSHOT"
+  lazy val _version = "3.0.1"
 
   lazy val _organization = "org.scalikejdbc"
 
