287c50dcd0f114e8f8f3a6caa8f368db99e565ab
xuwei-k <6b656e6a69@gmail.com>
2017-06-15 15:22:33 +0900

    support java.time.LocalDateTime in mapper-generator
    (cherry picked from commit 94ae3cb61f6a7666294662d7dd715cfd198a0d0a)

--- scalikejdbc-mapper-generator-core/src/main/scala/scalikejdbc/mapper/DateTimeClass.scala
+++ scalikejdbc-mapper-generator-core/src/main/scala/scalikejdbc/mapper/DateTimeClass.scala
@@ -4,9 +4,10 @@ object DateTimeClass {
   case object JodaDateTime extends DateTimeClass("org.joda.time.DateTime")
   case object ZonedDateTime extends DateTimeClass("java.time.ZonedDateTime")
   case object OffsetDateTime extends DateTimeClass("java.time.OffsetDateTime")
+  case object LocalDateTime extends DateTimeClass("java.time.LocalDateTime")
 
   private[scalikejdbc] val all = Set(
-    JodaDateTime, ZonedDateTime, OffsetDateTime
+    JodaDateTime, ZonedDateTime, OffsetDateTime, LocalDateTime
   )
 
   private[scalikejdbc] val map: Map[String, DateTimeClass] =
