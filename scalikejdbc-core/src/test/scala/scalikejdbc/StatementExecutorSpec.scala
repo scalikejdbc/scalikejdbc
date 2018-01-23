ab38dc6bde088f2b2bdf428253bc84d8925d0d61
zaneli <shun.otani@gmail.com>
2018-01-23 20:21:48 +0900

    add spec for print sqlString

--- scalikejdbc-core/src/test/scala/scalikejdbc/StatementExecutorSpec.scala
+++ scalikejdbc-core/src/test/scala/scalikejdbc/StatementExecutorSpec.scala
@@ -1,9 +1,10 @@
 package scalikejdbc
 
 import org.scalatest._
+import org.scalatest.mockito.MockitoSugar
 import java.sql.PreparedStatement
 
-class StatementExecutorSpec extends FlatSpec with Matchers {
+class StatementExecutorSpec extends FlatSpec with Matchers with MockitoSugar {
 
   behavior of "StatementExecutor"
 
@@ -13,6 +14,16 @@ class StatementExecutorSpec extends FlatSpec with Matchers {
     val params: Seq[Any] = Nil
     val instance = new StatementExecutor(underlying, template, DBConnectionAttributes(), params)
     instance should not be null
+  }
+
+  it should "print sql string" in {
+    val underlying: PreparedStatement = mock[PreparedStatement]
+    val template: String = "select id, name from members where id = ? and name = ?"
+    val params: Seq[Any] = Seq(1, "name1")
+    val instance = new StatementExecutor(underlying, template, DBConnectionAttributes(), params)
+    val m = instance.getClass.getDeclaredMethod("sqlString$lzycompute")
+    m.setAccessible(true)
+    m.invoke(instance) should equal("select id, name from members where id = 1 and name = 'name1'")
   }
 
 }
