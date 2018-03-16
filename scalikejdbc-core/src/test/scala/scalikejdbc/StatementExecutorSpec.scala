05b734fc150691dc56b32fd14b720f56949609a2
Kazuhiro Sera <seratch@gmail.com>
2018-03-16 15:56:43 +0900

    Modify #663 code to enable future maintainers to write some tests on the part

--- scalikejdbc-core/src/test/scala/scalikejdbc/StatementExecutorSpec.scala
+++ scalikejdbc-core/src/test/scala/scalikejdbc/StatementExecutorSpec.scala
@@ -29,4 +29,22 @@ class StatementExecutorSpec extends FlatSpec with Matchers with MockitoSugar {
     m.apply() should equal("select id, name from members where id = 1 and name = 'name1'")
   }
 
+  it should "have PrintableQueryBuilder inside" in {
+    {
+      val sql = StatementExecutor.PrintableQueryBuilder.build(
+        template = "select id, name from users where id = ? and name = ?",
+        settingsProvider = SettingsProvider.default,
+        params = Seq(123, "Alice"))
+      sql should equal("select id, name from users where id = 123 and name = 'Alice'")
+    }
+
+    {
+      val sql = StatementExecutor.PrintableQueryBuilder.build(
+        template = "select                       * from users where id = ?",
+        settingsProvider = SettingsProvider.default,
+        params = Seq(123))
+      sql should equal("select * from users where id = 123")
+    }
+  }
+
 }
