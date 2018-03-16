7ac9e37f1772fed9ddd1a83273b7b54becb33de1
Kazuhiro Sera <seratch@gmail.com>
2018-03-16 14:54:10 +0900

    Add more tests for request #842

--- scalikejdbc-core/src/test/scala/scalikejdbc/LogSpec.scala
+++ scalikejdbc-core/src/test/scala/scalikejdbc/LogSpec.scala
@@ -2,6 +2,7 @@ package scalikejdbc
 
 import org.scalatest._
 import org.slf4j._
+import org.mockito.Mockito.{ mock, verify, times, when }
 
 class LogSpec extends FlatSpec with Matchers {
 
@@ -11,6 +12,22 @@ class LogSpec extends FlatSpec with Matchers {
     val logger: Logger = LoggerFactory.getLogger(classOf[LogSpec])
     val instance = new Log(logger)
     instance should not be null
+  }
+
+  it should "have withLevel methods" in {
+    val ex = new RuntimeException("This is a sample error")
+
+    val logger: Logger = mock(classOf[Logger])
+    when(logger.isDebugEnabled()).thenReturn(true)
+
+    val log = new Log(logger)
+    log.isDebugEnabled = true
+
+    log.withLevel('debug)("Hi")
+    log.withLevel('debug)("Hi Hi", ex)
+
+    verify(logger, times(1)).debug("Hi")
+    verify(logger, times(1)).debug("Hi Hi", ex)
   }
 
 }
