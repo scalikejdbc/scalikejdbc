c4ad802e4be3a73f5535bf64ad6d972a83cad28e
Kazuhiro Sera <seratch@gmail.com>
2018-03-24 13:01:31 +0900

    version 3.2.2

--- scripts/release_all.sh
+++ scripts/release_all.sh
@@ -3,7 +3,19 @@
 cd `dirname $0`/..
 rm -rf */target
 
-sbt 'set scalaVersion := "2.12.5"' \
+sbt 'set scalaVersion := "2.13.0-M3"' \
+  clean \
+  "project core" 'set scalaVersion := "2.13.0-M3"' publishSigned \
+  "project config" 'set scalaVersion := "2.13.0-M3"' publishSigned \
+  "project interpolation-macro" 'set scalaVersion := "2.13.0-M3"' publishSigned \
+  "project interpolation" 'set scalaVersion := "2.13.0-M3"' publishSigned \
+  "project library" 'set scalaVersion := "2.13.0-M3"' publishSigned \
+  "project joda-time" 'set scalaVersion := "2.13.0-M3"' publishSigned \
+  "project streams" 'set scalaVersion := "2.13.0-M3"' publishSigned \
+  "project syntax-support-macro" 'set scalaVersion := "2.13.0-M3"' publishSigned \
+  "project test" 'set scalaVersion := "2.13.0-M3"' publishSigned \
+  "project mapper-generator-core" 'set scalaVersion := "2.13.0-M3"' publishSigned \
+  'set scalaVersion := "2.12.5"' \
   clean \
   "project core" 'set scalaVersion := "2.12.5"' publishSigned \
   "project config" 'set scalaVersion := "2.12.5"' publishSigned \
