f91c92576ff5fdeaf4b222ff2a38c401a2f88d88
Sean Sullivan <ssullivan@gilt.com>
2018-06-11 12:25:49 -0700

    override JDBCSettings toString
    Always redact the JDBC password.

--- scalikejdbc-core/src/test/scala/scalikejdbc/JDBCSettingsSpec.scala
+++ scalikejdbc-core/src/test/scala/scalikejdbc/JDBCSettingsSpec.scala
@@ -7,12 +7,13 @@ class JDBCSettingsSpec extends FlatSpec with Matchers {
   behavior of "JDBCSettings"
 
   it should "be available" in {
-    val url: String = ""
-    val user: String = ""
-    val password: String = ""
-    val driverName: String = ""
+    val url: String = "myUrl"
+    val user: String = "myUser"
+    val password: String = "myPassword123456"
+    val driverName: String = "myDriverName"
     val instance = new JDBCSettings(url, user, password, driverName)
     instance should not be null
+    instance.toString should be ("JDBCSettings(myUrl,myUser,[REDACTED],myDriverName)")
   }
 
 }
