a555167ce33af1aa1859373c7cf044261a340162
xuwei-k <6b656e6a69@gmail.com>
2018-07-26 10:55:24 +0900

    remove deprecated SQLInterpolation object

--- scalikejdbc-interpolation/src/main/scala/scalikejdbc/SQLInterpolation.scala
+++ scalikejdbc-interpolation/src/main/scala/scalikejdbc/SQLInterpolation.scala
@@ -1,9 +1,5 @@
 package scalikejdbc
 
-@deprecated("'import SQLInterpolation._' is no longer required. Just 'import scalikejdbc._' works fine.", since = "2.0.0")
-object SQLInterpolation {
-}
-
 /**
  * SQLInterpolation full imports.
  */
