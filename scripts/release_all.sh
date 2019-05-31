bb7da9ac6866ac64f69d35c609fdf2678bd4534f
xuwei-k <6b656e6a69@gmail.com>
2019-06-01 08:22:00 +0900

    Scala 2.13.0-RC3

--- scripts/release_all.sh
+++ scripts/release_all.sh
@@ -3,18 +3,18 @@
 cd `dirname $0`/..
 rm -rf */target
 
-sbt 'set scalaVersion := "2.13.0-RC2"' \
+sbt 'set scalaVersion := "2.13.0-RC3"' \
   clean \
-  "project core" 'set scalaVersion := "2.13.0-RC2"' publishSigned \
-  "project config" 'set scalaVersion := "2.13.0-RC2"' publishSigned \
-  "project interpolation-macro" 'set scalaVersion := "2.13.0-RC2"' publishSigned \
-  "project interpolation" 'set scalaVersion := "2.13.0-RC2"' publishSigned \
-  "project library" 'set scalaVersion := "2.13.0-RC2"' publishSigned \
-  "project joda-time" 'set scalaVersion := "2.13.0-RC2"' publishSigned \
-  "project streams" 'set scalaVersion := "2.13.0-RC2"' publishSigned \
-  "project syntax-support-macro" 'set scalaVersion := "2.13.0-RC2"' publishSigned \
-  "project test" 'set scalaVersion := "2.13.0-RC2"' publishSigned \
-  "project mapper-generator-core" 'set scalaVersion := "2.13.0-RC2"' publishSigned \
+  "project core" 'set scalaVersion := "2.13.0-RC3"' publishSigned \
+  "project config" 'set scalaVersion := "2.13.0-RC3"' publishSigned \
+  "project interpolation-macro" 'set scalaVersion := "2.13.0-RC3"' publishSigned \
+  "project interpolation" 'set scalaVersion := "2.13.0-RC3"' publishSigned \
+  "project library" 'set scalaVersion := "2.13.0-RC3"' publishSigned \
+  "project joda-time" 'set scalaVersion := "2.13.0-RC3"' publishSigned \
+  "project streams" 'set scalaVersion := "2.13.0-RC3"' publishSigned \
+  "project syntax-support-macro" 'set scalaVersion := "2.13.0-RC3"' publishSigned \
+  "project test" 'set scalaVersion := "2.13.0-RC3"' publishSigned \
+  "project mapper-generator-core" 'set scalaVersion := "2.13.0-RC3"' publishSigned \
   'set scalaVersion := "2.12.8"' \
   clean \
   "project core" 'set scalaVersion := "2.12.8"' publishSigned \
