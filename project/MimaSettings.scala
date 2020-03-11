f889f363539785c0fc0d253bbbe0781c72062bef
xuwei-k <6b656e6a69@gmail.com>
2020-03-11 11:45:24 +0900

    Update dependencies

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -1,6 +1,6 @@
 import sbt._, Keys._
 import com.typesafe.tools.mima.plugin.MimaPlugin
-import com.typesafe.tools.mima.plugin.MimaKeys.{mimaPreviousArtifacts, mimaReportBinaryIssues}
+import com.typesafe.tools.mima.plugin.MimaKeys._
 
 /*
  * MiMa settings of ScalikeJDBC libs.
@@ -22,7 +22,9 @@ object MimaSettings {
   //
   val previousVersions = Set(0).map(patch => s"3.4.$patch")
 
-  val mimaSettings = MimaPlugin.mimaDefaultSettings ++ Seq(
+  val mimaSettings = Seq(
+    ThisBuild / mimaReportSignatureProblems := true,
+    ThisBuild / mimaFailOnNoPrevious := false,
     mimaPreviousArtifacts := {
       CrossVersion.partialVersion(scalaVersion.value) match {
         case Some((2, scalaMajor)) if scalaMajor <= 13 =>
