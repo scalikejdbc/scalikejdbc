94dedbfcd98c6430f783d9b0b1e3d1dbdf61b65d
xuwei-k <6b656e6a69@gmail.com>
2020-03-17 07:51:19 +0900

    Scala 2.12.11

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -e
 
 if [[ ${TRAVIS_SCALA_VERSION} = "scripted-test" ]]; then
   sbt \
-  '++ 2.12.10! -v' \
+  '++ 2.12.11! -v' \
   publishLocal \
   '++ 2.13.1! -v' \
   root213/publishLocal \
