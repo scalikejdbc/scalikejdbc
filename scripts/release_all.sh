1df13c750033693a1483b40c15c0b3e2427f6e2f
xuwei-k <6b656e6a69@gmail.com>
2020-07-25 16:26:18 +0900

    Scala 2.12.12

--- scripts/release_all.sh
+++ scripts/release_all.sh
@@ -15,18 +15,18 @@ sbt 'set scalaVersion := "2.13.3"' \
   "project syntax-support-macro" 'set scalaVersion := "2.13.3"' publishSigned \
   "project test" 'set scalaVersion := "2.13.3"' publishSigned \
   "project mapper-generator-core" 'set scalaVersion := "2.13.3"' publishSigned \
-  'set scalaVersion := "2.12.11"' \
+  'set scalaVersion := "2.12.12"' \
   clean \
-  "project core" 'set scalaVersion := "2.12.11"' publishSigned \
-  "project config" 'set scalaVersion := "2.12.11"' publishSigned \
-  "project interpolation-macro" 'set scalaVersion := "2.12.11"' publishSigned \
-  "project interpolation" 'set scalaVersion := "2.12.11"' publishSigned \
-  "project library" 'set scalaVersion := "2.12.11"' publishSigned \
-  "project joda-time" 'set scalaVersion := "2.12.11"' publishSigned \
-  "project streams" 'set scalaVersion := "2.12.11"' publishSigned \
-  "project syntax-support-macro" 'set scalaVersion := "2.12.11"' publishSigned \
-  "project test" 'set scalaVersion := "2.12.11"' publishSigned \
-  "project mapper-generator-core" 'set scalaVersion := "2.12.11"' publishSigned
+  "project core" 'set scalaVersion := "2.12.12"' publishSigned \
+  "project config" 'set scalaVersion := "2.12.12"' publishSigned \
+  "project interpolation-macro" 'set scalaVersion := "2.12.12"' publishSigned \
+  "project interpolation" 'set scalaVersion := "2.12.12"' publishSigned \
+  "project library" 'set scalaVersion := "2.12.12"' publishSigned \
+  "project joda-time" 'set scalaVersion := "2.12.12"' publishSigned \
+  "project streams" 'set scalaVersion := "2.12.12"' publishSigned \
+  "project syntax-support-macro" 'set scalaVersion := "2.12.12"' publishSigned \
+  "project test" 'set scalaVersion := "2.12.12"' publishSigned \
+  "project mapper-generator-core" 'set scalaVersion := "2.12.12"' publishSigned
 
 `dirname $0`/release_sbt_plugins.sh
 
--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -e
 
 if [[ ${TRAVIS_SCALA_VERSION} = "scripted-test" ]]; then
   sbt \
-  '++ 2.12.11! -v' \
+  '++ 2.12.12! -v' \
   publishLocal \
   '++ 2.13.3! -v' \
   root213/publishLocal \
