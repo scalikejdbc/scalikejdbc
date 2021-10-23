25b379cac1b5cd7cb39a122179742f1c3a477b6f
xuwei-k <6b656e6a69@gmail.com>
2021-10-23 10:58:04 +0900

    Update scala versions

--- scripts/release_all.sh
+++ scripts/release_all.sh
@@ -3,30 +3,30 @@
 cd `dirname $0`/..
 rm -rf */target
 
-sbt 'set scalaVersion := "2.13.4"' \
+sbt 'set scalaVersion := "2.13.6"' \
   clean \
-  "project core" 'set scalaVersion := "2.13.4"' publishSigned \
-  "project config" 'set scalaVersion := "2.13.4"' publishSigned \
-  "project interpolation-macro" 'set scalaVersion := "2.13.4"' publishSigned \
-  "project interpolation" 'set scalaVersion := "2.13.4"' publishSigned \
-  "project library" 'set scalaVersion := "2.13.4"' publishSigned \
-  "project joda-time" 'set scalaVersion := "2.13.4"' publishSigned \
-  "project streams" 'set scalaVersion := "2.13.4"' publishSigned \
-  "project syntax-support-macro" 'set scalaVersion := "2.13.4"' publishSigned \
-  "project test" 'set scalaVersion := "2.13.4"' publishSigned \
-  "project mapper-generator-core" 'set scalaVersion := "2.13.4"' publishSigned \
-  'set scalaVersion := "2.12.12"' \
+  "project core" 'set scalaVersion := "2.13.6"' publishSigned \
+  "project config" 'set scalaVersion := "2.13.6"' publishSigned \
+  "project interpolation-macro" 'set scalaVersion := "2.13.6"' publishSigned \
+  "project interpolation" 'set scalaVersion := "2.13.6"' publishSigned \
+  "project library" 'set scalaVersion := "2.13.6"' publishSigned \
+  "project joda-time" 'set scalaVersion := "2.13.6"' publishSigned \
+  "project streams" 'set scalaVersion := "2.13.6"' publishSigned \
+  "project syntax-support-macro" 'set scalaVersion := "2.13.6"' publishSigned \
+  "project test" 'set scalaVersion := "2.13.6"' publishSigned \
+  "project mapper-generator-core" 'set scalaVersion := "2.13.6"' publishSigned \
+  'set scalaVersion := "2.12.15"' \
   clean \
-  "project core" 'set scalaVersion := "2.12.12"' publishSigned \
-  "project config" 'set scalaVersion := "2.12.12"' publishSigned \
-  "project interpolation-macro" 'set scalaVersion := "2.12.12"' publishSigned \
-  "project interpolation" 'set scalaVersion := "2.12.12"' publishSigned \
-  "project library" 'set scalaVersion := "2.12.12"' publishSigned \
-  "project joda-time" 'set scalaVersion := "2.12.12"' publishSigned \
-  "project streams" 'set scalaVersion := "2.12.12"' publishSigned \
-  "project syntax-support-macro" 'set scalaVersion := "2.12.12"' publishSigned \
-  "project test" 'set scalaVersion := "2.12.12"' publishSigned \
-  "project mapper-generator-core" 'set scalaVersion := "2.12.12"' publishSigned
+  "project core" 'set scalaVersion := "2.12.15"' publishSigned \
+  "project config" 'set scalaVersion := "2.12.15"' publishSigned \
+  "project interpolation-macro" 'set scalaVersion := "2.12.15"' publishSigned \
+  "project interpolation" 'set scalaVersion := "2.12.15"' publishSigned \
+  "project library" 'set scalaVersion := "2.12.15"' publishSigned \
+  "project joda-time" 'set scalaVersion := "2.12.15"' publishSigned \
+  "project streams" 'set scalaVersion := "2.12.15"' publishSigned \
+  "project syntax-support-macro" 'set scalaVersion := "2.12.15"' publishSigned \
+  "project test" 'set scalaVersion := "2.12.15"' publishSigned \
+  "project mapper-generator-core" 'set scalaVersion := "2.12.15"' publishSigned
 
 `dirname $0`/release_sbt_plugins.sh
 
