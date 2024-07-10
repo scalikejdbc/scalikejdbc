c5864d89ad459f63720f5e978b263d557e491015
kenji yoshida <6b656e6a69@gmail.com>
2024-07-11 08:01:44 +0900

    add `getTables` method (#2259)

--- scalikejdbc-core/src/test/scala/scalikejdbc/metadata/TableSpec.scala
+++ scalikejdbc-core/src/test/scala/scalikejdbc/metadata/TableSpec.scala
@@ -9,13 +9,21 @@ class TableSpec extends AnyFlatSpec with Matchers {
 
   it should "be available" in {
     val name: String = ""
+    val catalog: String = ""
     val schema: String = ""
     val description: String = ""
     val columns: List[Column] = Nil
     val foreignKeys: List[ForeignKey] = Nil
     val indices: List[Index] = Nil
-    val instance =
-      new Table(name, schema, description, columns, foreignKeys, indices)
+    val instance = new Table(
+      name,
+      catalog,
+      schema,
+      description,
+      columns,
+      foreignKeys,
+      indices
+    )
     instance should not be null
   }
 
