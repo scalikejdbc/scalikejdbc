26c380733ec87d9c0209eb5f009f357bf16c81a9
xuwei-k <6b656e6a69@gmail.com>
2026-09-15 12:46:24 +0900

    Add explicit types. prepare Scala 3.10

--- scalikejdbc-interpolation/src/test/scala/scalikejdbc/QueryInterfaceSpec.scala
+++ scalikejdbc-interpolation/src/test/scala/scalikejdbc/QueryInterfaceSpec.scala
@@ -221,7 +221,7 @@ class QueryInterfaceSpec
         }
         batchInsertQuery
           .batch(Seq(3, "Coffee", 90), Seq(4, "Chocolate", 200))
-          .apply()
+          .apply[List]()
 
         withSQL { delete.from(Product).where.in(pc.id, Seq(3, 4)) }.update
           .apply()
@@ -243,7 +243,7 @@ class QueryInterfaceSpec
           }
           withSQL {
             insert.into(Product).namedValues(params.columnsAndPlaceholders*)
-          }.batch(params.batchParams*).apply()
+          }.batch(params.batchParams*).apply[List]()
 
           withSQL { delete.from(Product).where.in(pc.id, Seq(3, 4)) }.update
             .apply()
